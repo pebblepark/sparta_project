e840617c761389835464e944b7f3e48f36c34133
pebblepark <subling02@gmail.com>
2022-05-09 16:30:36 +0900

    #2 얼굴 영역 탐지

--- week3/main.py
+++ week3/main.py
@@ -16,6 +16,33 @@ while True:
     if ret == False:
         break
 
+    height, width, channel = img.shape
+    img = cv2.resize(img, dsize=(1000, int(height/width * 1000)))
+
+    h, w, c = img.shape
+    # 이미지 전처리하기
+    blob = cv2.dnn.blobFromImage(img, size=(300, 300), mean=(104., 177., 123.))
+
+    # 얼굴 영역 탐지 모델로 추론하기
+    facenet.setInput(blob)
+    dets = facenet.forward()
+
+    # 각 얼굴에 대해서 반복문 돌기
+    for i in range(dets.shape[2]):
+        confidence = dets[0, 0, i, 2]
+
+        if confidence < 0.5:
+            continue
+
+        # 사각형 꼭지점 찾기
+        x1 = int(dets[0, 0, i, 3] * w)
+        y1 = int(dets[0, 0, i, 4] * h)
+        x2 = int(dets[0, 0, i, 5] * w)
+        y2 = int(dets[0, 0, i, 6] * h)
+
+        # 사각형 그리기
+        cv2.rectangle(img, pt1=(x1, y1), pt2=(x2, y2), thickness=2, color=(0, 255, 0))
+
     cv2.imshow('result', img)
     if cv2.waitKey(1) == ord('q'):
         break
