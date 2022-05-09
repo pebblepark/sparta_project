13caefc71c582d4d3224c498b83827f9417a0293
pebblepark <subling02@gmail.com>
2022-05-09 15:08:51 +0900

    #1 숙제2. 이미지 가로로 잘라 반반 적용하기

--- week2/main3.py
+++ week2/main3.py
@@ -2,16 +2,16 @@ import cv2
 import numpy as np
 
 net = cv2.dnn.readNetFromTorch('models/instance_norm/feathers.t7')
+net2 = cv2.dnn.readNetFromTorch('models/instance_norm/the_scream.t7')
 
 img = cv2.imread('imgs/hw.jpg')
 
 h, w, c = img.shape
 
-cropped_img = img[144:368, 480:812]
-# img = cv2.resize(img, dsize=(500, int(h / w * 500)))
+img = cv2.resize(img, dsize=(int(w / h * 300), 300))
 
 MEAN_VALUE = [103.939, 116.779, 123.680]
-blob = cv2.dnn.blobFromImage(cropped_img, mean=MEAN_VALUE)
+blob = cv2.dnn.blobFromImage(img, mean=MEAN_VALUE)
 
 net.setInput(blob)
 output = net.forward()
@@ -22,7 +22,20 @@ output += MEAN_VALUE
 output = np.clip(output, 0, 255)
 output = output.astype('uint8')
 
-img[144:368, 480:812] = output
+net2.setInput(blob)
+output2 = net2.forward()
+
+output2 = output2.squeeze().transpose((1, 2, 0))
+
+output2 += MEAN_VALUE
+output2 = np.clip(output2, 0, 255)
+output2 = output2.astype('uint8')
+
+output = output[0:150, :]
+output2 = output2[150:300, :]
+
+output3 = np.concatenate([output, output2], axis=0)
 
 cv2.imshow('img', img)
+cv2.imshow('result', output3)
 cv2.waitKey(0)
