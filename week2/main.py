5cc061e24b24e636d3e194c8b0af4bf6c29fe9c4
pebblepark <subling02@gmail.com>
2022-05-09 14:31:48 +0900

    #1 이미지 전처리

--- week2/main.py
+++ week2/main.py
@@ -6,5 +6,17 @@ net = cv2.dnn.readNetFromTorch('models/eccv16/starry_night.t7')
 
 img = cv2.imread('imgs/01.jpg')
 
+h, w, c = img.shape
+
+img = cv2.resize(img, dsize=(500, int(h / w * 500)))
+
+print(img.shape)
+
+# 전처리
+MEAN_VALUE = [103.939, 116.779, 123.680]
+blob = cv2.dnn.blobFromImage(img, mean=MEAN_VALUE)
+
+print(blob.shape)
+
 cv2.imshow('img', img)
 cv2.waitKey(0)
