7241d6553725c319725254e46dd97a2be3af366f
pebblepark <subling02@gmail.com>
2022-05-10 11:28:56 +0900

    %5 뼈대 만들기

---
+++ week5/main.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+proto = 'models/colorization_deploy_v2.prototxt'
+weights = 'models/colorization_release_v2.caffemodel'
+
+net = cv2.dnn.readNetFromCaffe(proto, weights)
+
+pts_in_hull = np.load('models/pts_in_hull.npy')
+pts_in_hull = pts_in_hull.transpose().reshape(2, 313, 1, 1).astype(np.float32)
+net.getLayer(net.getLayerId('class8_ab')).blobs = [pts_in_hull]
+
+net.getLayer(net.getLayerId('conv8_313_rh')).blobs = [np.full((1, 313), 2.606, np.float32)]
