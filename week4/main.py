e4ad9b963a5b36f2dc0f5304c5ff1d5e4f58b5d6
pebblepark <subling02@gmail.com>
2022-05-10 09:38:50 +0900

    #3 스티커 붙이기

--- week4/main.py
+++ week4/main.py
@@ -16,12 +16,25 @@ while True:
     print("number of faces detected:", len(dets))
 
     for det in dets:
-        x1 = det.left()
-        y1 = det.top()
-        x2 = det.right()
-        y2 = det.bottom()
+        x1 = det.left() - 40
+        y1 = det.top() - 50
+        x2 = det.right() + 40
+        y2 = det.bottom() + 30
 
-        cv2.rectangle(img, pt1=(x1, y1), pt2=(x2, y2), color=(255,0,0), thickness=2)
+        # cv2.rectangle(img, pt1=(x1, y1), pt2=(x2, y2), color=(255,0,0), thickness=2)
+
+        try:
+            overlay_img = sticker_img.copy()
+
+            overlay_img = cv2.resize(overlay_img, dsize=(x2 - x1, y2 - y1))
+
+            overlay_alpha = overlay_img[:, :, 3:4] / 255.0
+            background_alpha = 1.0 - overlay_alpha
+
+            img[y1:y2, x1:x2] = overlay_alpha * overlay_img[:, :, :3] + background_alpha * img[y1:y2, x1:x2]
+
+        except:
+            pass
 
     cv2.imshow('result', img)
     if cv2.waitKey(1) == ord('q'):
