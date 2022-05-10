55d703e32b258c4e514a1fd808999b777209c42f
pebblepark <subling02@gmail.com>
2022-05-10 09:37:59 +0900

    #3 얼굴 영역 탐지

--- week4/main.py
+++ week4/main.py
@@ -12,6 +12,17 @@ while True:
     if ret == False:
         break
 
+    dets = detector(img)
+    print("number of faces detected:", len(dets))
+
+    for det in dets:
+        x1 = det.left()
+        y1 = det.top()
+        x2 = det.right()
+        y2 = det.bottom()
+
+        cv2.rectangle(img, pt1=(x1, y1), pt2=(x2, y2), color=(255,0,0), thickness=2)
+
     cv2.imshow('result', img)
     if cv2.waitKey(1) == ord('q'):
         break
